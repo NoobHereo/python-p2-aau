67bf3a60977f445d7fa0106ef1917b37b4c07b4f
Marcus0108 <38943632+Marcus0108@users.noreply.github.com>
2022-05-19 15:26:14 +0200

    Yes

--- Webots/Emil's World/PyController.py
+++ Webots/Emil's World/PyController.py
@@ -14,30 +14,7 @@ if __name__ == "__main__":
     
     parts.rightMotor.setPosition(float('inf')); parts.rightMotor.setVelocity(0.0)
     
-    linearVelocity = data.wheelRadius * data.maxSpeed
-    
-    durationSide = data.lengthSide/linearVelocity
-    
-    startTime = robot.getTime()
-    
-    angleOfRotation = 6.28/data.numSide
-    rateOfRotation = (2 * linearVelocity)/data.wheelDistance
-    durationTurn = angleOfRotation/rateOfRotation
-    
-    # Rotation start and end time calculations
-    rotStartTime = startTime + durationSide; rotEndTime = rotStartTime + durationTurn
-    
     # Main loop:
     # - perform simulation steps until Webots is stopping the controller
     while robot.step(data.timestep) != -1:
-        currentTime = robot.getTime()
-        PyLib.Right()
-
-        if rotStartTime < currentTime < rotEndTime:
-            PyLib.Left()        
-        elif currentTime > rotEndTime:
-            rotStartTime = currentTime + durationSide; rotEndTime = rotStartTime + durationTurn
-            parts.redLed.set(0)
-
-        PyLib.Forward()
-        PyLib.Brushing()
+        PyLib.Drive()
--- Webots/Emil's World/PyLib.py
+++ Webots/Emil's World/PyLib.py
@@ -9,35 +9,66 @@ class Data:
     wheelRadius = 0.1
     wheelDistance = 0.46
 
+    linearVelocity = wheelRadius * maxSpeed
+    angleOfRotation = 6.28/numSide
+    rateOfRotation = (2 * linearVelocity)/wheelDistance
+    durationTurn = angleOfRotation/rateOfRotation
+    
+    durationSide = lengthSide/linearVelocity
+    rotStartTime = robot.getTime() + durationSide; rotEndTime = rotStartTime + durationTurn
+
+    def setSpeed(speed):
+        maxSpeed = speed
+
 data = Data()
 
 class Parts:
         # LED shows whparts is used, ie. the brush component or laser component
-        redLed = robot.getDevice('led') 
+        redLed = robot.getDevice('led')
         leftMotor = robot.getDevice('left_motor')
         rightMotor = robot.getDevice('right_motor')
         # brush = robot.getDevice('brush')
-        # laser = robot.getDevice('laser')
+        # laser = robot.getDevice('laser') 
 
 parts = Parts()
 
+# Using the brush component
+class Brushing:
+    def setState(state): # state should be an int. 0 = off, 1 = on.
+        parts.redLed.set(state)
+    
+# Using the laser component
+class Eradicate:
+    def setState(state): # state should be an int. 0 = off, 1 = on.
+        parts.redLed.set(state)
+
+# Drive forward
 class Forward:
     leftSpeed = data.maxSpeed
     rightSpeed = data.maxSpeed 
 
+# Drive Left
 class Left:
     leftSpeed = -data.maxSpeed
     rightSpeed = data.maxSpeed
-    parts.redLed.set(0)
+    parts.redLed.set(0)   
 
-class Brushing:
-    parts.redLed.set(1)
-    
-class Eradicate:
-    parts.redLed.set(1)
-    
-
+# Drive Right
 class Right:
     leftSpeed = data.maxSpeed
     rightSpeed = -data.maxSpeed
     parts.redLed.set(0)
+
+class Drive:
+    def __init__(drive, leftSpeed, rightSpeed, rotEndTime, rotStartTime):
+        drive.currentTime = robot.getTime()
+        Right()
+        
+        if rotStartTime < robot.getTime() < rotEndTime:
+            Left()
+        elif drive.currentTime > rotEndTime:
+            rotStartTime = robot.getTime() + data.durationSide; rotEndTime = rotStartTime + data.durationTurn
+            parts.redLed.set(0)
+        
+        Forward()
+        Brushing()
